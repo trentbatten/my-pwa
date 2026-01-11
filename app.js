59eb7fb5672e23ea7963983291047d07c199b7f6
trentbatten <trentbatten8@gmail.com>
2026-01-11 17:30:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ function sendData() {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
 }
+
+<h2 id="datetime"></h2>
+
+<label>
+  Monthly Spending Goal ($)
+  <input type="number" id="monthlyGoal" maxlength="6" />
+</label>
+
+<br><br>
+
+<label>
+  Money Spent ($)
+  <input type="number" id="moneySpent" maxlength="6" />
+</label>
+
+<br><br>
+
+<h3>
+  Daily Spending Goal: $
+  <span id="dailyGoal">--</span>
+</h3>
